271f1001dcdf7dcefe5778ffb0e8916dd5a62ee4
ObadiahBorms02 <31545309+ObadiahBorms02@users.noreply.github.com>
2018-11-27 09:48:33 -0800

    Add files via upload

---
+++ set_1.py
@@ -0,0 +1,16 @@
+#set_1.py okb
+alpha = {"a", "bb", "ccc", "dddd", "eeeee", "ffffff"
+for x in alpha:
+	#print(alpha[x]) #uncomment this line an try 
+	print(x)
+print(sorted(alpha))
+
+"""
+Look at what is going on here. 
+A set can not be indexed.
+for x in alpha looks at all the elements in 
+the set and assigns one at a time to the x 
+variable.
+The following is an excellent resource.
+https://www.programiz.com/ python-programming/se
+"""
---
+++ set_2.py
@@ -0,0 +1,13 @@
+#sets_2.py okb
+alpha = {"a", "b", "c", "d", "e", "f"}
+for x in alph:
+	print(x," ", end="")
+alpha.add("x")
+alpha.add("y")
+alpha.add("z")
+print("add to set alpha")
+for x in alpha:
+	print(x," ",end="")
+"""
+The add function is used to add elements.
+"""
